c6007302c77809ee8228af202fe8afc4c571368a
VMA9 <111783796+VMA9@users.noreply.github.com>
2022-09-16 17:38:27 +0300

    Update OdevVucutKitleIndeksiHesaplayanProgram.java

--- src/OdevVucutKitleIndeksiHesaplayanProgram.java
+++ src/OdevVucutKitleIndeksiHesaplayanProgram.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class OdevVucutKitleIndeksiHesaplayanProgram {
+import java.util.Scanner;
+public class OdevVucutKitleIndeksiHesaplayanProgram {
+    public static void main(String[] args) {
+        double kg , m , VKI;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen Boyunuzu Giriniz (Metre Cinsinden) : " );
+        m = input.nextDouble();
+
+        System.out.print("Lütfen Kilonuzu Giriniz : " );
+        kg = input.nextDouble();
+
+        VKI = kg / (m * m);
+        System.out.print("Vücut Kitle İndeksiniz : " + VKI);
+
+    }
 }
